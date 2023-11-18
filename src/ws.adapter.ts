32521e6ffa617566c1ab29052421c703b3d93307
Cristian Pinzon <faykris28@gmail.com>
2023-11-18 13:17:32 -0500

    add deployed frontend url

--- src/ws.adapter.ts
+++ src/ws.adapter.ts
@@ -4,7 +4,7 @@ import * as socketio from 'socket.io';
 
 export class WsAdapter extends IoAdapter {
   createIOServer(port: number, options?: socketio.ServerOptions): any {
-    const server = super.createIOServer(port, { ...options, cors: { origin: 'http://localhost:4200', methods: ['GET', 'POST', 'PUT', 'DELETE'] } });
+    const server = super.createIOServer(port, { ...options, cors: { origin: 'https://weather-monitoring-front.netlify.app', methods: ['GET', 'POST', 'PUT', 'DELETE'] } });
     return server;
   }
 }
