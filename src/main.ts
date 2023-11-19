9dc8385a0fb54dae19a7e878f8e2677c51d368cd
Cristian Pinzon <faykris28@gmail.com>
2023-11-18 21:57:03 -0500

    use require cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from './ws.adapter';
 import * as dotenv from 'dotenv';
+var cors = require('cors')
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  //app.use(cors())
+  //app.enableCors();
+  app.use(cors())
 
   //app.use(function (request, response, next) {
   //  response.header("Access-Control-Allow-Origin", "*");
